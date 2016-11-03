171d780c41e5509220f43ee2ae42a603de0661e7
Mikhail Kulichkov <mikhail.kulichkov@gmail.com>
2016-11-03 23:10:15 +0300

    Adding Tapatalk API Swift-file

---
+++ mamask_app/mamask_app/Tapatalk.swift
@@ -0,0 +1,12 @@
+//
+//  Tapatalk.swift
+//  mamask_app
+//
+//  Created by Mikhail Kulichkov on 03/11/16.
+//  Copyright © 2016 Mikhail Kulichkov. All rights reserved.
+//
+
+import Foundation
+import wpxmlrpc
+
+
